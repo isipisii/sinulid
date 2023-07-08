33a98b2a858b938c9ce5e0c4f523dd17d4a93ebd
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-08 21:47:04 +0800

    created routes for post

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -3,6 +3,14 @@ import  express from "express";
 
 const router = express.Router()
 
-router.post("/new", PostControllers.createPost);
+router.post("/", PostControllers.createPost);
+
+router.patch("/:postId", PostControllers.updatePost)
+
+router.get("/user-posts/:userId", PostControllers.getUserPosts)
+
+router.get("/", PostControllers.getPosts)
+
+router.delete("/:postId", PostControllers.deletePost)
 
 export default router 

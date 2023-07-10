3ec88a9be76bfc682724da6094fc8d3d62cc771a
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-10 22:30:09 +0800

    configured some colors to be use

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        matteBlack: "#0C0C0D",
+        primaryText:"",
+        lightText: "#929191",
+        cta: "#FAF906",
+        
+      },
+      
+    },
   },
   plugins: [],
 }

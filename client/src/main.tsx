1e574f9cc20f6d9a40358c275b74aef40513d79b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-10 22:31:41 +0800

    font

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store.ts";
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

ff5b02daacdc8cfe6c9ddad37bd8187574059400
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-12 23:09:02 +0800

    created types for new envs

--- server/src/util/validateEnv.ts
+++ server/src/util/validateEnv.ts
@@ -4,5 +4,8 @@ import { port, str } from "envalid/dist/validators";
 export default cleanEnv(process.env, {
     MONGO_DB_CON_STR: str(),
     PORT: port(),
-    JWT_SECRET: str()
+    JWT_SECRET: str(),
+    CLOUDINARY_API_KEY: str(),
+    CLOUD_NAME: str(),
+    CLOUDINARY_SECRET: str()
 })

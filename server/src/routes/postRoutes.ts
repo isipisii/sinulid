ac482a0e94a04549a991f3ce233ad30119c0af62
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-12 23:08:05 +0800

    created new routes for post

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,16 +1,23 @@
 import * as PostControllers from "../controllers/postControllers"
 import  express from "express";
+import upload from "../util/multer"
 
 const router = express.Router()
 
-router.post("/", PostControllers.createPost);
+router.post("/", upload.single("image"), PostControllers.createPost);
 
-router.patch("/:postId", PostControllers.updatePost)
+router.patch("/:postId", upload.single("image"), PostControllers.updatePost)
 
 router.get("/user-posts/:userId", PostControllers.getUserPosts)
 
 router.get("/", PostControllers.getPosts)
 
 router.delete("/:postId", PostControllers.deletePost)
+
+router.get("/likes/:postId", PostControllers.checkIfUserLikes)
+
+router.patch("/like/:postId", PostControllers.likePost)
+
+router.patch("/unlike/:postId", PostControllers.unlikePost)
 
 export default router 

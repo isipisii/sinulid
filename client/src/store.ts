1c5bdfa3cfba67e64689534f781bf9826797c36a
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-15 14:40:44 +0800

    rtk set up

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './features/auth/authSlice'
+import { api } from './services/api'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: authSlice.reducer,
+    [api.reducerPath]: api.reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 })
 
+setupListeners(store.dispatch)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch

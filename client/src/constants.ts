146162d46ae5a26bff2490bcf13110020d082e75
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-15 14:43:10 +0800

    Create constants.ts

---
+++ client/src/constants.ts
@@ -0,0 +1,36 @@
+export const logInFields = [
+    {
+        inputName: "email",
+        placeholder: "Email",
+        inputType: "email",
+    },
+    {
+        inputName: "password",
+        placeholder: "Password",
+        inputType: "password",
+    },
+];
+
+
+export const signUpFields = [
+    {
+        inputName: "username",
+        placeholder:"Username",
+        inputType: "text"
+    },
+    {
+        inputName: "email",
+        placeholder: "Email",
+        inputType: "email"
+    },
+    {
+        inputName: "password",
+        placeholder: "Password",
+        inputType: "password",
+    },
+    {
+        inputName: "password",
+        placeholder: "Confirm password",
+        inputType: "password",
+    }
+]

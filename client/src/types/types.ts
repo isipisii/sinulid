fb8f0432c08f13488a7ae806b7953562e2bbc768
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-15 14:40:22 +0800

    created new types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,4 +1,4 @@
-export type SignUpCredential = {
+export type SignUpCredentials = {
     username: string
     email: string
     password: string
@@ -7,9 +7,27 @@ export type SignUpCredential = {
 export type LogInCredentials = {
     email: string;
     password: string;
-  };
+};
+
+export type UpdateUserInfo = {
+    bio: string
+    link: string
+    username: string
+    image: File | null
+}
+
+
 
 export type User = {
     _id: string
     username: string
+    displayed_picture: {
+        url: string,
+        cloudinary_id: string
+    }
+    bio: string
+    link: string
+    following: string[]
+    followers: string[]
 }
+

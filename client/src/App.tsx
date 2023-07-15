67b3c9e904d53b6853700316b69771b32d1c0b9a
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-15 14:43:56 +0800

    changed to rtk query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,49 @@
-import { FC } from "react"
-import { Routes, Route } from "react-router-dom"
-import SignUp from "./pages/SignUp"
-import LogIn from "./pages/LogIn"
-import Home from "./pages/Home"
-import { useAppSelector, useAppDispatch } from "./features/app/hooks"
-import { User } from "./types/types"
-import { setUser } from "./features/auth/authSlice"
-import Protected from "./components/Protected"
+import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import { useGetAuthenticatedUserMutation } from "./services/authApi";
+import { useAppSelector, useAppDispatch } from "./features/app/hooks";
+import { setUser } from "./features/auth/authSlice";
+import Protected from "./components/Protected";
 
 const App: FC = () => {
-  const { token } = useAppSelector((state) => state.auth)
-  const dispatch = useAppDispatch()
+  const { token } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+  const [getAuthenticatedUserInfo] = useGetAuthenticatedUserMutation()
 
-  // gets the user who logs in along with the token to authenticate 
+  // gets the user who logs in along with the token to authenticate
   async function getUserInfo(): Promise<void> {
     try {
-      const response = await fetch("http://localhost:5000/users/", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-      })
-      const data: User = await response.json()
-      dispatch(setUser(data))
-      console.log(data)
+      const authenticatedUser = await getAuthenticatedUserInfo(token).unwrap()
+
+      if(authenticatedUser){
+        dispatch(setUser(authenticatedUser))
+      }
     } catch (error) {
       console.error(error)
-    }
-}
+    } 
+  }
 
   return (
-    <Routes>
-      <Route path="/" element={
-        <Protected isSignedIn={token}>
-            <Home getUserInfo={getUserInfo}/>
-        </Protected>} 
-      />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp/>} />
-    </Routes>
-  )
-}
+    <>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Protected isSignedIn={token}>
+              <Home getUserInfo={getUserInfo} />
+            </Protected>
+          }
+        />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;

3877343e05426b3e1d5ce77bd0421156424cc097
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-16 22:59:25 +0800

    styled sidebar

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,21 +1,59 @@
-import { FC } from 'react'
-import { User } from '../types/types'
+import { FC } from "react";
+import { User } from "../types/types";
+import { Link } from "react-router-dom";
 
 interface ISideBar {
-    userInfo: User | null
-    handleLogout: () => void
+  userInfo: User | null;
 }
 
-const SideBar: FC<ISideBar> = ({ userInfo, handleLogout }) => {
-
+const SideBar: FC<ISideBar> = ({ userInfo }) => {
   return (
-    <aside className='border-slate-600 border p-4 w-[400px] h-[500px] rounded-md'>
-        <div className='flex items-center'>
-            <h1 className='text-white font-bold text-[2rem]'>{userInfo?.username}</h1>
-            <button onClick={handleLogout} className='bg-red-500 text-white'>Log out</button>
-        </div>  
-    </aside>
-  ) 
-}
+    <aside className="w-[30%] hidden md:block h-[100vh]">
+      <div className="w-auto flex flex-col justify-between h-[85vh] mt-[70px] max-w-[300px]">
+        {/*current user profile */}
+        <div className="flex items-center gap-3 bg-secondaryBg flex-col rounded-3xl p-4 w-full">
+          {/* name, image, bio */}
+          <div className="flex flex-col items-center w-full gap-3">
+            <img
+              src="https://greenacresportsmed.com.au/wp-content/uploads/2018/01/dummy-image.jpg"
+              alt=""
+              className="w-[70px] rounded-full"
+            />
+            <div className="flex flex-col gap-2">
+              <div>
+                <h1 className="text-white font-medium text-base text-center">
+                  {userInfo?.username}
+                </h1>
+                <p className="text-lightText text-xs text-center">@alessandroo</p>
+              </div>
+              <p className="text-white text-xs text-center">
+                Bioooooooo <br /> bio
+              </p>
+            </div>
+          </div>
 
-export default SideBar
+          {/* following and followers */}
+          <div className="flex w-full border-t border-[#514e4eb7]">
+            <div className="p-4 w-full flex-col flex items-center">
+              <h5 className="text-white font-semibold">100</h5>
+              <p className="text-lightText text-xs">Following</p>
+            </div>
+            <div className="p-4 w-full  flex-col flex items-center">
+              <h5 className="text-white font-semibold">100</h5>
+              <p className="text-lightText text-xs">Followers</p>
+            </div>
+          </div>
+
+          <div className="border-t border-[#514e4eb7] w-full text-center pt-4">
+            <Link to="/profile" className="text-cta font-semibold text-xs">
+              View Profile
+            </Link>
+          </div>
+        </div>
+        {/*end of current user profile */}
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;

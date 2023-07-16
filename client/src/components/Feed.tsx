db74b3d02fbe29bdcb7cc93e18dfd31ec7975a4b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-16 22:58:31 +0800

    styled and added some post cards

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,16 +1,22 @@
-import { FC } from 'react'
-
-// interface ISideBar {
-//     userInfo: User | null
-//     handleLogout: () => void
-// }
+import { FC, useEffect } from 'react'
+import PostCard from './PostCard'
+import CreatePostForm from './CreatePostForm'
+import { useGetPostQuery } from '../services/postApi'
 
 const Feed: FC= () => {
+  const {data: posts, refetch: refetchPosts} = useGetPostQuery()
+
+  useEffect(() => {
+    refetchPosts()
+  },[])
 
   return (
-    <section className='border-slate-600 border flex-grow h-[100vh] rounded-md overflow-y-auto'>
-        <div className='h-[200vh] bg-slate-600'>
-
+    <section className= "w-full pt-[70px] flex items-center flex-col justify-center gap-3">
+        <CreatePostForm />
+        <div className='flex flex-col gap-4 w-full max-w-[600px]'>
+          {posts && posts.map((post, index) => (
+            <PostCard key={index} post={post}/>
+          ))}
         </div>
     </section>
   ) 

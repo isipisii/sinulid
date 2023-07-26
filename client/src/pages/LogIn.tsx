4512e78cd8caba025bfb263313086195b0bf7a01
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-26 22:50:47 +0800

    added show password and styles

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -1,17 +1,19 @@
-import { FC } from "react";
+import { JSX, useState } from "react";
 import { LogInCredentials } from "../types/types";
 import { useNavigate, Link } from "react-router-dom";
 import { useAppDispatch } from "../features/app/hooks";
 import { useLogInMutation } from "../services/authApi";
 import { setToken } from "../features/auth/authSlice";
 import { useForm } from 'react-hook-form';
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
-const LogIn: FC = () => {
+const LogIn = (): JSX.Element => {
+  const [showPassword, setShowPassword] = useState<boolean>(false)
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const [logIn] = useLogInMutation();
   const { register, handleSubmit, formState: { errors } } = useForm<LogInCredentials>()
-  const errorStyle = "text-red-500 text-[.7rem]"
+  const errorStyle = "text-red-500 text-[.7rem] mt-[-.5rem]"
 
   async function handleLogIn(logInCredentials: LogInCredentials): Promise<void> {
     try {
@@ -33,7 +35,7 @@ const LogIn: FC = () => {
         onSubmit={handleSubmit(handleLogIn)}
       >
         <h1 className="text-white font-semibold text-[1.5rem] self-start">
-          Log in to Sinulid
+          <span className="font-light text-lightText">Log in to</span> Sinulid
         </h1>
         <div className="flex justify-center flex-col gap-3 w-full">
           {/* email */}
@@ -41,32 +43,39 @@ const LogIn: FC = () => {
             placeholder='Email'
             type='email'
             className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-            focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.email ? "border-red-500 focus:ring-red-500" : null}`}
+            focus:outline-none focus:border-none focus:ring-1 sm:text-sm bg-transparent ${errors.email ? "border-red-500 focus:ring-red-500" : "focus:ring-white"}`}
             {...register("email", {required: true})}
           />
           {errors.email && errors.email.type === "required"  && (<p className={errorStyle}>Email is required</p>)}
 
           {/* password */}
-          <input 
-            placeholder='Password'
-            type='password'
-            className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-            focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.password ? "border-red-500 focus:ring-red-500" : null}`}
-            {...register("password", {required: true, minLength: 8})}
-          />
+          <div className="relative">
+            <input 
+              placeholder='Password'
+              type={showPassword ? "text" : "password"}
+              className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 pr-10 shadow-sm 
+              focus:outline-none focus:border-non focus:ring-1 sm:text-sm bg-transparent ${errors.password ? "border-red-500 focus:ring-red-500" : "focus:ring-white"}`}
+              {...register("password", {required: true, minLength: 8})}
+            />
+            {showPassword ? 
+              <FiEyeOff className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setShowPassword(false)} />  
+                  : 
+              <FiEye className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setShowPassword(true)}/>
+            }
+          </div>
           {errors.password && errors.password.type === "required"  && (<p className={errorStyle}>Password is required</p>)}
           {errors.password && errors.password.type === "minLength"  && (<p className={errorStyle}>Password should be more than 8 characters</p>)}
           
         </div>
         <button
           type="submit"
-          className="bg-cta w-full p-2 rounded-sm text-matteBlack font-bold text-sm"
+          className="bg-gradient-to-br from-[#393939] to-[#ffffff] w-full p-2 rounded-sm text-matteBlack font-bold text-sm"
         >
           Log in
         </button>
         <p className="text-lightText text-xs">
           Don't have an account?{" "}
-          <Link to="/signup" className="text-cta">
+          <Link to="/signup" className="text-white">
             Sign up
           </Link>
         </p>
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { JSX, useState } from 'react'
 import { SignUpCredentials } from '../types/types'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
@@ -6,14 +6,16 @@ import { useAppDispatch } from '../features/app/hooks'
 import { setToken } from '../features/auth/authSlice'
 import { useSignUpMutation } from '../services/authApi'
 import { useForm } from 'react-hook-form';
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
-
-const SignUp: FC = () => {
+const SignUp = (): JSX.Element => {
+    const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false)
+    const [isConfirmPasswordShown, setIsConfirmPasswordShown] = useState<boolean>(false)
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
     const [signUp] = useSignUpMutation()
     const { register, handleSubmit, getValues, formState: { errors } } = useForm<SignUpCredentials>()
-    const errorStyle = "text-red-500 text-[.7rem]"
+    const errorStyle = "text-red-500 text-[.7rem] mt-[-.5rem]"
 
     async function handleSignUp(signUpCredentials: SignUpCredentials): Promise<void> {
         try {
@@ -30,15 +32,14 @@ const SignUp: FC = () => {
 
     return (
     <div className='h-[100vh] flex items-center justify-center bg-matteBlack'>
-        {/* <form className='flex items-center justify-center flex-col gap-6 border border-slate-600 p-5 rounded-md w-[400px]' onSubmit={handleSignUp}> */}
         <form className='flex items-center justify-center flex-col gap-6 border border-borderColor p-5 rounded-md w-[400px]' onSubmit={handleSubmit(handleSignUp)}>
-            <h1 className='text-white font-semibold text-[1.5rem] self-start'>Join Sinulid today</h1>
+            <h1 className='text-white font-semibold text-[1.5rem] self-start'><span className='font-light text-lightText'>Join</span> Sinulid <span className='font-light text-lightText'>today</span></h1>
             <div className='flex justify-center flex-col gap-3 w-full'>
             {/* username */}
             <input
                 placeholder='Username'
                 className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-                focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.username ? "border-red-500 focus:ring-red-500" : null}`}
+                focus:outline-none focus:border-none focus:ring-1 sm:text-sm bg-transparent ${errors.username ? "border-red-500 focus:ring-red-500" : "focus:ring-white"}`}
                 {...register("username", {required: true, maxLength: 15})}
             />
             {errors.username && errors.username.type === "required"  && (<p className={errorStyle}>Username is required</p>)}
@@ -49,47 +50,58 @@ const SignUp: FC = () => {
                 placeholder='Email'
                 type='email'
                 className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-                focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.email ? "border-red-500 focus:ring-red-500" : null}`}
+                focus:outline-none focus:border-non focus:ring-1 sm:text-sm bg-transparent ${errors.email ? "border-red-500 focus:ring-red-500" : "focus:ring-white"}`}
                 {...register("email", {required: true})}
             />
             {errors.email && errors.email.type === "required"  && (<p className={errorStyle}>Email is required</p>)}
 
             {/* password */}
-            <input 
-                placeholder='Password'
-                type='password'
-                className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-                focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.password ? "border-red-500 focus:ring-red-500" : null}`}
-                {...register("password", {required: true, minLength: 8})}
-            />
+            <div className='relative'>
+                <input 
+                    placeholder='Password'
+                    type={isPasswordShown ? "text" : "password"}
+                    className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 pr-10 shadow-sm 
+                    focus:outline-none focus:border-non focus:ring-1 sm:text-sm bg-transparent ${errors.password ? "border-red-500 focus:ring-red-500" : "focus:ring-white"}`}
+                    {...register("password", {required: true, minLength: 8})}
+                />
+                {isPasswordShown ? 
+                <FiEyeOff className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setIsPasswordShown(false)} />  
+                    : 
+                <FiEye className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setIsPasswordShown(true)}/>
+                }
+            </div>
             {errors.password && errors.password.type === "required"  && (<p className={errorStyle}>Password is required</p>)}
             {errors.password && errors.password.type === "minLength"  && (<p className={errorStyle}>Password should be more than 8 characters</p>)}
 
             {/* confirm password */}
-            <input 
-                placeholder='Confirm password'
-                type='password'
-                className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 shadow-sm 
-                focus:outline-none focus:border-none focus:ring-cta focus:ring-1 sm:text-sm bg-transparent ${errors.confirmPassword ? "border-red-500 focus:ring-red-500" : null}`}
-                {...register("confirmPassword", {
-                    required: true,
-                    validate: {
-                        matchPassword: (value) => {
-                            const { password } = getValues()
-                            return password === value || "Password didn't match";
-                        }
-                    },
-                })}
-            />
+            <div className='relative'>
+                <input 
+                    placeholder='Confirm password'
+                    type={isConfirmPasswordShown ? "text" : "password"}
+                    className={`placeholder:text-[#726e6e] text-white block w-full border border-borderColor rounded-sm p-2 pr-10 shadow-sm 
+                    focus:outline-none focus:border-non focus:ring-1 sm:text-sm bg-transparent ${errors.confirmPassword ? "border-red-500 focus:ring-red-500" : "focus:ring-white "}`}
+                    {...register("confirmPassword", {
+                        required: true,
+                        validate: {
+                            matchPassword: (value) => {
+                                const { password } = getValues()
+                                return password === value || "Password didn't match";
+                            }
+                        },
+                    })}
+                />
+                {isConfirmPasswordShown ? 
+                <FiEyeOff className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setIsConfirmPasswordShown(false)} />  
+                    : 
+                <FiEye className="absolute top-0 bottom-0 right-3 my-auto text-lightText text-[1rem] cursor-pointer" onClick={() => setIsConfirmPasswordShown(true)}/>
+                }
+            </div>
             {errors.confirmPassword && errors.confirmPassword.type === "required"  && (<p className={errorStyle}>Confirm password is required</p>)}
             {errors.confirmPassword  && (<p className={errorStyle}>{errors.confirmPassword.message}</p>)}
 
-            {/* {signUpFields.map((field, index) => (
-                <input name={field.inputName} type={field.inputType} placeholder={field.placeholder} className='focus:border-[#ffffff81] text-sm border border-[#d3d3d354] text-white rounded-sm bg-[transparent] w-full p-2' onChange={handleOnChange} key={index}/>
-            ))} */}
             </div>  
-            <button type='submit' className='bg-cta w-full p-2 rounded-sm text-matteBlack font-bold text-sm'>Sign Up</button>
-            <p className='text-lightText text-xs'>Got an account?  <Link to="/login" className='text-cta'>Log in</Link></p>
+            <button type='submit' className='bg-gradient-to-br from-[#393939] to-[#ffffff] w-full p-2 rounded-sm text-matteBlack font-bold text-sm'>Sign Up</button>
+            <p className='text-lightText text-xs'>Got an account?  <Link to="/login" className='text-white'>Log in</Link></p>
         </form>
     </div>
 )}

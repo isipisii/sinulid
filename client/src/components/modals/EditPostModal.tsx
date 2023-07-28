a09885485fe10ecd52cbeca219169f4e90eb07e2
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-28 23:40:02 +0800

    add: created modal for editing a certain post

---
+++ client/src/components/modals/EditPostModal.tsx
@@ -0,0 +1,28 @@
+import { JSX } from "react";
+import { useAppSelector, useAppDispatch } from "../../features/app/hooks";
+import { setPostToEdit } from "../../features/post/postSlice";
+import CreateAndEditPostForm from "../CreateAndEditPostForm";
+import { IoMdClose } from "react-icons/io";
+
+const EditPostModal = (): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const { postToEdit } = useAppSelector((state) => state.post);
+
+  return (
+    <>
+      {postToEdit  && (
+        <div className="bg-[#000000bd] backdrop-blur-sm fixed w-[100vw] h-[100vh] z-20 top-0 left-0 flex items-center justify-center">
+          {/* close icon */}
+          <p className="top-5 right-5 text-[#525151] text-[1.5rem] absolute p-3 font-thin rounded-full bg-[#252424ce] hover:bg-[#424141ba] cursor-pointer" onClick={() => dispatch(setPostToEdit(null))}>
+            <IoMdClose />
+          </p>
+          <div className="w-[90%] max-w-[600px]">
+            <CreateAndEditPostForm isEditing={true} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default EditPostModal;

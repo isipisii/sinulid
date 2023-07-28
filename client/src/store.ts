84d6f8afd8ae51bc85ae4934bca67dd0834e5e16
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-28 23:47:01 +0800

    add: add the userProfileSlice reducer

--- client/src/store.ts
+++ client/src/store.ts
@@ -3,11 +3,13 @@ import { authSlice } from './features/auth/authSlice'
 import { api } from './services/api'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import { postSlice } from './features/post/postSlice'
+import userProfileSlice from './features/user/userProfileSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     post: postSlice.reducer,
+    userProfile: userProfileSlice.reducer,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),

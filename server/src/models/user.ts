34bf804c57d2e1414805efc4169bd2bf6241f6f8
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-28 23:46:33 +0800

    add: add name property

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,6 +6,10 @@ const userSchema = new Schema({
         required: true,
         unique: true 
     },
+    name: {
+        type: String, 
+        required: true,
+    },
     email: { 
         type: String, 
         required: true, 

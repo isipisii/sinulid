fbb9187b407c293c2243deb9a1e7d36ad39a1b91
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-07-30 22:44:14 +0800

    add: new page for future use

---
+++ client/src/pages/Activity.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react'
+
+
+const Activity = (): JSX.Element => {
+  return (
+    <div className='h-[100vh] w-full bg-matteBlack'>Activity</div>
+  )
+}
+
+export default Activity

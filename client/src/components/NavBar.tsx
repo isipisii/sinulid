9eb6a0ce2607fa83a22be9aff354db746a83bff4
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-01 10:29:09 +0800

    add: add sign up for those who are not authenticated

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = (): JSX.Element => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useAppDispatch();
-  const { user } = useAppSelector((state) => state.auth);
+  const { user, token } = useAppSelector((state) => state.auth);
 
   function handleLogout() {
     localStorage.removeItem("token");
@@ -17,12 +17,17 @@ const NavBar = (): JSX.Element => {
     navigate("/login");
   }
 
+  // function test(){
+  //   document.body.classList.add('body-dark');
+  // }
+
   return (
     <>
       {location.pathname !== "/signup" && location.pathname !== "/login" && (
-          <nav className="w-full backdrop-blur-sm bg-[#101010de] fixed z-20 flex items-center justify-center border-borderColor border-b">
-            <div className=" flex items-center justify-between p-4 w-full h-auto">
-              <h1 className="text-white font-bold">sinolid.</h1>
+        <nav className="w-full backdrop-blur-sm bg-[#101010de] fixed z-20 flex items-center justify-center border-borderColor border-b">
+          <div className=" flex items-center justify-between p-4 w-full h-auto">
+            <h1 className="text-white font-bold">sinolid.</h1>
+            {user && token ? (
               <div className="border border-borderColor p-2 flex gap-2 items-center rounded-md">
                 <img
                   src={
@@ -36,9 +41,12 @@ const NavBar = (): JSX.Element => {
                 />
                 <p className="text-white text-xs">{user?.username}</p>
               </div>
-            </div>
-          </nav>
-        )}
+            ) : (
+              <h2 className="text-white text-xs border border-borderColor p-2 rounded-md cursor-pointer" onClick={() => navigate("/signup")}>Sign up</h2>
+            )}
+          </div>
+        </nav>
+      )}
     </>
   );
 };

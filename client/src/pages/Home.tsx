fe92836d116d6c84d4579189c1e5a5e1b7f75b0d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-04 14:40:20 +0800

    add: add data fetching for user's posts and reposts

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { FC, useEffect } from "react";
 import { setPosts } from "../features/post/postSlice";
 import { useLazyGetPostsQuery } from "../services/postApi";
-import { useAppDispatch } from "../features/app/hooks";
+import { useAppDispatch, useAppSelector } from "../features/app/hooks";
 import Feed from "../components/Feed";
 import CreateAndEditPostForm from "../components/CreateAndEditPostForm";
+import { useLazyGetUserPostsAndRepostsQuery } from "../services/authAndUserApi";
+import { setUserPostsAndReposts } from "../features/user/userProfileSlice";
 
 interface IHome {
   getUserInfo: () => Promise<void>
@@ -12,6 +14,8 @@ interface IHome {
 const Home: FC<IHome> = ({ getUserInfo }) => {
   const dispatch = useAppDispatch();
   const [getLazyPostsQuery] = useLazyGetPostsQuery();
+  const [getPostsAndRepostsQuery] = useLazyGetUserPostsAndRepostsQuery();
+  const { user: authenticatedUser } = useAppSelector(state => state.auth)
 
   async function getPosts() {
     try {
@@ -24,6 +28,23 @@ const Home: FC<IHome> = ({ getUserInfo }) => {
     }
   }
 
+
+  useEffect(() => {
+    async function getUserRepostsAndPosts(): Promise<void> {
+      if (!authenticatedUser) return;
+
+      try {
+        const userPostsAndRepostsPayload = await getPostsAndRepostsQuery(authenticatedUser?._id).unwrap();
+        if(userPostsAndRepostsPayload){
+          dispatch(setUserPostsAndReposts(userPostsAndRepostsPayload))
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUserRepostsAndPosts()
+  }, [authenticatedUser?._id, dispatch, getPostsAndRepostsQuery, authenticatedUser ]);
+
   // get the user info  and posts once rendered
   useEffect(() => {
     getPosts();
@@ -32,10 +53,12 @@ const Home: FC<IHome> = ({ getUserInfo }) => {
   }, []);
 
   return (
-    <section className="bg-matteBlack w-full flex items-center justify-center">
+    <section className="bg-matteBlack w-full flex items-center justify-center pb-[100px]">
       <div className="max-w-[1400px] h-auto w-full flex justify-center gap-4 md:ml-[120px] md:mr-[50px] lg:ml-[250px]">
-        <main className="w-full pt-[90px] flex items-center flex-col justify-center gap-3 sm:mx-[50px] max-w-[700px]">
-          <CreateAndEditPostForm isEditing={false} />
+        <main className="w-full pt-[90px] flex items-center flex-col justify-center sm:mx-[50px] max-w-[700px]">
+          <div className="border-b border-borderColor w-full">
+            <CreateAndEditPostForm isEditing={false} />
+          </div>
           <Feed />
         </main>
         {/* may ikaag */}

2d78354f03c28887e8796fb12dec52ba58456e31
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-05 21:32:37 +0800

    .

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,24 +1,19 @@
 import { FC } from "react";
-import PostCard from "./PostCard";
+import { MemoizedPostAndRepostCard } from "./PostAndRepostCard";
 import { useAppSelector } from "../features/app/hooks";
-import { Repost } from "../types/types";
+import { filteredUserReposts} from "../util/filteredUserReposts";
 
 const Feed: FC = () => {
   const { token, user } = useAppSelector((state) => state.auth);
   const { userPostsAndReposts } = useAppSelector((state) => state.userProfile);
   const { posts } = useAppSelector((state) => state.post);
-
-  // filtered reposts of the current user inorder to check if the post from feed is already reposted or not
-  const userReposts = userPostsAndReposts.filter((item) => {
-    if (item.type === "repost") {
-      const repost = item as Repost;
-      return repost;
-    }
-  }) as Repost[];
+  // helper function that filters the posts and reposts of the user and returns an array of user reposts
+  const userReposts = filteredUserReposts(userPostsAndReposts)
 
   return (
     <div className="flex flex-col w-full">
       {posts.map((post) => {
+        // will check every post to identify if it is being reposted by the current user
         const isReposted = userReposts.some(
           (repost) =>
             repost.post._id === post._id &&
@@ -26,13 +21,12 @@ const Feed: FC = () => {
         );
 
         return (
-          <PostCard
+          <MemoizedPostAndRepostCard
             key={post._id}
             post={post}
             token={token}
             authenticatedUser={user}
             isReposted={isReposted}
-            userReposts={userReposts}
           />
         );
       })}

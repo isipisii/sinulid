81c583750f7e7439f12930117341bf6cb4cd987c
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-06 10:57:24 +0800

    created a util function for toast

---
+++ client/src/util/showToast.ts
@@ -0,0 +1,34 @@
+import { toast } from "react-hot-toast";
+
+export const showToast = (message: string, isError?: boolean) => {
+
+  if (isError) {
+    return toast.error(message, {
+      style: {
+        border: "1px solid #353131f6",
+        padding: "12px",
+        fontSize: "12px",
+        color: "#FFFAEE",
+        background: "#101010",
+      },
+      iconTheme: {
+        primary: "#D00000",
+        secondary: "#FFFAEE",
+      },
+    });
+  }
+
+  return toast.success(message, {
+    style: {
+      border: "1px solid #353131f6",
+      padding: "12px",
+      fontSize: "12px",
+      color: "#FFFAEE",
+      background: "#101010",
+    },
+    iconTheme: {
+      primary: "#38b000",
+      secondary: "#101010",
+    },
+  });
+};

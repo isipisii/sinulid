fd2d76e5faf63d3138d8a87e38e838f110903db8
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-09 15:53:58 +0800

    rendered the post replies and post details

--- client/src/pages/PostAndRepliesPage.tsx
+++ client/src/pages/PostAndRepliesPage.tsx
@@ -1,24 +1,27 @@
-import { JSX, useEffect, useState } from "react";
+import { JSX, useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../features/app/hooks";
-import { useParams } from "react-router-dom";
-import { useLazyGetSinglePostQuery, useLazyGetPostRepliesQuery, useDeletePostReplyMutation } from "../services/postApi";
-import { setSinglePost } from "../features/post/postSlice";
+import { useParams, useNavigate } from "react-router-dom";
+import { useLazyGetSinglePostQuery,  useLazyGetRepliesQuery} from "../services/postApi";
+import { setSinglePost, setReplies } from "../features/post/postSlice";
+import { filteredUserReposts } from "../util/filteredUserReposts";
 
+import { IoIosArrowBack } from "react-icons/io"
 import { MemoizedPostAndRepostCard } from "../components/PostAndRepostCard";
-import { Reply } from "../types/types";
-import ReplyCard from "../components/ReplyCard";
 
 const PostAndReplies = (): JSX.Element => {
-  const { user: authenticatedUser, token } = useAppSelector(
-    (state) => state.auth
-  );
-  const [postReplies, setPostReplies] = useState<Reply[]>([]);
-  const { post } = useAppSelector((state) => state.post);
-  const dispatch = useAppDispatch();
-  const [getSinglePostQuery] = useLazyGetSinglePostQuery();
-  const [getPostReplies] = useLazyGetPostRepliesQuery()
-  const [deletePostReplyMutation] = useDeletePostReplyMutation();
   const { postId, username } = useParams();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate()
+
+  const { user: authenticatedUser, token } = useAppSelector((state) => state.auth);
+  const { userPostsAndReposts } = useAppSelector((state) => state.userProfile);
+  const { post, replies } = useAppSelector((state) => state.post);
+
+  const [getSinglePostQuery] = useLazyGetSinglePostQuery();
+  const [getReplies] = useLazyGetRepliesQuery()
+ 
+  const userReposts = filteredUserReposts(userPostsAndReposts)
+  const isReposted = userReposts.some((repost) => repost.post._id === post?._id && repost.repost_creator._id === authenticatedUser?._id)
 
   useEffect(() => {
     async function getSinglePostAndReplies() {
@@ -29,52 +32,14 @@ const PostAndReplies = (): JSX.Element => {
         if (post) {
           dispatch(setSinglePost(post));
         }
-        const replies = await getPostReplies(post?._id).unwrap();
-        if (replies) {
-          setPostReplies(replies);
-        }
+        const replies = await getReplies(post._id).unwrap()
+        dispatch(setReplies(replies))
       } catch (error) {
         console.error(error);
       }
     }
     getSinglePostAndReplies();
-  }, [postId, username, dispatch, getPostReplies, getSinglePostQuery]);
-  console.log(post);
-  console.log(postReplies)
-
-  // const handleSubmitReply = useCallback(
-  //   async (e: FormEvent<HTMLFormElement>) => {
-  //     e.preventDefault();
-  //     if (!postData) return;
-  //     try {
-  //       const replyRes = await createPostReply({
-  //         postId: postData?._id,
-  //         token,
-  //         content: reply,
-  //       }).unwrap();
-  //       const updatedReplies = [...postReplies, replyRes];
-  //       setPostReplies(updatedReplies);
-  //       setReply("");
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   },
-  //   [reply, postData, createPostReply, setPostReplies, token, postReplies]
-  // );
-
-  async function handleDeleteReply(replyId: string): Promise<void> {
-    if (!authenticatedUser) return;
-
-    try {
-      await deletePostReplyMutation({ token, replyId });
-      const updatedReplies = postReplies.filter(
-        (reply) => reply._id !== replyId
-      );
-      setPostReplies(updatedReplies);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  }, [postId, username, dispatch, getSinglePostQuery, getReplies]);
 
   return (
     <section className="bg-matteBlack w-full flex items-center justify-center pb-[100px]">
@@ -84,27 +49,43 @@ const PostAndReplies = (): JSX.Element => {
         } flex justify-center`}
       >
         <main className="w-full pt-[90px] flex items-center flex-col justify-center sm:mx-[50px] max-w-[700px]">
-          {post && (
-            <MemoizedPostAndRepostCard
-              post={post}
-              token={token}
-              authenticatedUser={authenticatedUser}
-            />
-          )}
-            {postReplies.length === 0 ? (
-              <p className="text-lightText text-xs font-light">
+          <div className="w-full relative mb-4">
+            {authenticatedUser && <p onClick={() => navigate(-1)} className=" cursor-pointer hover:bg-[#4e4a4a48] ease-in-out duration-300 p-2 rounded-md text-white absolute  my-auto left-2 md:left-0 text-sm flex items-center gap-1"><IoIosArrowBack/> Back</p>}
+            <h1 className="text-white font-medium text-center text-[1.3rem]">Thread</h1>
+          </div>
+
+          <div className="w-full">
+            {post && (
+              <MemoizedPostAndRepostCard
+                post={post}
+                token={token}
+                authenticatedUser={authenticatedUser}
+                isReposted={isReposted}
+                isRootPost={true}
+              />
+            )}
+          </div>
+
+            {replies.length === 0 ? (
+              <p className="text-lightText text-xs font-light mt-4">
                 No replies yet
               </p>
             ) : (
-              postReplies && (
+              replies && (
                 <div className="flex flex-col items-center w-full">
-                  {postReplies.map((reply, index) => (
-                    <ReplyCard
-                      reply={reply}
-                      key={index}
-                      handleDeleteReply={handleDeleteReply}
-                    />
-                  ))}
+                  {replies.map((reply, index) => {
+                    const isReposted = userReposts.some((repost) => repost.post._id === reply?._id && repost.repost_creator._id === authenticatedUser?._id)
+                    
+                    return (
+                       <MemoizedPostAndRepostCard
+                        key={index}
+                        post={reply}
+                        token={token}
+                        authenticatedUser={authenticatedUser}
+                        isReposted={isReposted}
+                      />
+                    )
+                  })}
                 </div>
               )
             )}

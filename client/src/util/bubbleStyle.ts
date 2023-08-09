7a91158c919b3d12fdda1c595ee910c35e5759bd
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-09 16:03:56 +0800

    adjusted the image size

--- client/src/util/bubbleStyle.ts
+++ client/src/util/bubbleStyle.ts
@@ -20,13 +20,13 @@ export function bubbleStyleUserImageWhoReplied(idx: number, imageUrls: string[])
   if (imageUrls.length > 2) {
     switch (idx) {
       case 0:
-        style = "absolute bottom-4 right-0 h-[18px] w-[18px]";
+        style = "absolute bottom-4 right-0 h-[19px] w-[19px]";
         break;
       case 1:
         style = "absolute bottom-3 left-1 h-[15px] w-[15px]";
         break;
       case 2:
-        style = "absolute bottom-0 right-2  h-[14px] w-[14px]";
+        style = "absolute bottom-1 right-3  h-[10px] w-[10px]";
         break;
     }
   }

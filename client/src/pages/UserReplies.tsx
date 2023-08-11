f6f8e2111bf42b022de19c250fe9399e13409a3b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-11 19:57:03 +0800

    mapped the user replies

--- client/src/pages/UserReplies.tsx
+++ client/src/pages/UserReplies.tsx
@@ -1,14 +1,21 @@
 import { FC } from 'react'
 import { User } from '../types/types'
+import RootPostAndReplyCard from '../components/cards/RootPostAndReplyCard'
+import { useGetUserRepliesQuery } from '../services/authAndUserApi'
 
 interface IUserReplies {
   userProfileInfo: User | null
 }
 
-const UserReplies: FC<IUserReplies> = ( {userProfileInfo }) => {
-  console.log(userProfileInfo?._id)
+const UserReplies: FC<IUserReplies> = ({ userProfileInfo }) => {
+  const { data } = useGetUserRepliesQuery(userProfileInfo?._id ?? "")
+
   return (
-    <div>UserReplies</div>
+    <div>
+      {data?.map((reply) => (
+          <RootPostAndReplyCard post={reply} isRootPost={false} key={reply._id}/>
+      ))}
+    </div>
   )
 }
 

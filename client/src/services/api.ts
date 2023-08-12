5e2f33af4b275f213e4dfe5ee8301f7058b6d589
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-12 14:47:39 +0800

    .

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,5 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
-    endpoints: () => ({})
+    endpoints: () => ({}),
+    // tagTypes: ["Posts", "PostReplies", "Post"]
 })  

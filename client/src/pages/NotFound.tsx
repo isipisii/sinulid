004534f30f5af56aaa2203c43af7b0f2461cf451
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-08-15 12:22:51 +0800

    add: add not found page

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1 className='text-white text-[4rem]'>NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default NotFound
